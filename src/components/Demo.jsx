3805bafe6b7f5f62bed0a970b37d6f62d5e4ed9f
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-04-21 23:28:09 +0545

    Added a copy to clipboard feature

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -12,6 +12,7 @@ const Demo = () => {
     })
 
     const [allArticles, setAllArticles] = useState([]);
+    const [copied, setCopied] = useState("");
 
     useEffect(() => {
         const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
@@ -36,6 +37,14 @@ const Demo = () => {
         }
     }
 
+    const handleCopy = (copyUrl) => {
+        setCopied(copyUrl);
+        navigator.clipboard.writeText(copyUrl);
+        setTimeout(() => {
+            setCopied(false)
+        }, 3000);
+    }
+
     return (
         <section className="mt-16 w-full max-w-xl">
             {/* Search */}
@@ -50,8 +59,8 @@ const Demo = () => {
                 <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
                     {allArticles.map((item, index) => (
                         <div key={`link-${index}`} onClick={() => setArticle(item)} className="link_card">
-                            <div className="copy_btn">
-                                <img src={copy} alt="copy icon" className='w-[40%] h-[40%] object-contain' />
+                            <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                                <img src={copied === item.url ? tick : copy} alt="copy icon" className='w-[40%] h-[40%] object-contain' />
                             </div>
                             <p className="flex-1 font-santoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
                         </div>
